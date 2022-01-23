a8deeb335322d2e1267f1f5585081eb9f1eb1ab4
Silvia España <silviaespanagil@gmail.com>
2022-01-23 13:48:42 +0100

    Create FactsView component

---
+++ Femhoot/Femhoot/Facts/FactsView.swift
@@ -0,0 +1,44 @@
+//
+//  FactsView.swift
+//  Femhoot
+//
+//  Created by Silvia España on 23/1/22.
+//
+
+import SwiftUI
+
+struct FactsView: View {
+    
+    @StateObject var viewModel: FactsViewModel
+    
+    var body: some View {
+        
+        HStack {
+            
+            Text(viewModel.randomFact)
+                .font(.footnote)
+            Button(action: {
+                viewModel.getFact()
+            }){
+                
+            Image(systemName: viewModel.imageSystemName)
+                    .frame(width: 20, height: 20)
+                    .padding(5)
+                    .font(.footnote)
+                    .foregroundColor(Color.noWhite)
+                    .background(Color.lila)
+                    .clipShape(Circle())
+            }
+        }
+        .padding()
+        .onAppear {
+            viewModel.getFact()
+        }
+    }
+}
+
+struct FactsView_Previews: PreviewProvider {
+    static var previews: some View {
+        FactsView(viewModel: FactsViewModel())
+    }
+}
