efa3c2bfda8c53f28e991b9b0a1507e1dc86cc0d
Silvia España <silviaespanagil@gmail.com>
2022-01-23 11:39:46 +0100

    Generate color asset Create color extension with custom palette

---
+++ Femhoot/Femhoot/Extension/ColorExtension.swift
@@ -0,0 +1,16 @@
+//
+//  ColorExtension.swift
+//  Femhoot
+//
+//  Created by Silvia España on 23/1/22.
+//
+
+import SwiftUI
+
+public extension Color {
+    
+    static let pink = Color("Pink")
+    static let lila = Color("Lila")
+    static let lightBlue = Color("LightBlue")
+    static let noWhite = Color("NoWhite")
+}
