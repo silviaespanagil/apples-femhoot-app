be84afe7aa3c0b2334c9204b9b7b9a07aaa4910a
Silvia España <silviaespanagil@gmail.com>
2022-01-23 10:48:17 +0100

    Set up - initial commit

---
+++ Femhoot/Femhoot/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Femhoot
+//
+//  Created by Silvia España on 23/1/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Femhoot/Femhoot/FemhootApp.swift
@@ -0,0 +1,17 @@
+//
+//  FemhootApp.swift
+//  Femhoot
+//
+//  Created by Silvia España on 23/1/22.
+//
+
+import SwiftUI
+
+@main
+struct FemhootApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
